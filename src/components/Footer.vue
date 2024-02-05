5ad90bf336e80b24f87ed6b212452dfaddd976e1
Guilherme-HJA <guilherme.hjaraujo@proton.me>
2024-02-05 10:41:22 -0300

    Footer Component Fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
-    <strong>Veisla</strong> by
-    <a href="https://github.com/Guilherme-HJA">Guilherme Araujo</a>,
-    is made under <a href="https://opensource.org/licenses/mit-license.php">MIT License</a>.<br>
-    Source code can be found at <a href="https://github.com/Guilherme-HJA/veisla">Github</a>
+    <strong>Veisla</strong>, a <strong>NON - PROFITABLE</strong> open-source
+    project by <a href="https://github.com/Guilherme-HJA">Guilherme Araujo</a>,
+    is made under
+    <a href="https://opensource.org/licenses/mit-license.php">MIT License</a
+    >.<br />
+    Source code can be found at
+    <a href="https://github.com/Guilherme-HJA/veisla">Github</a>
   </div>
 </template>
 <style lang="scss" scoped>
 @import "../styles/variables";
 
 div {
-
   text-align: center;
   font-size: 1.1rem;
 
