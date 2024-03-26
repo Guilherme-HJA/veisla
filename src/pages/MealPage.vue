7293d2e40e1451c13a6fd5f889a9d73b12bdac99
Guilherme-HJA <guilherme.hjaraujo@proton.me>
2024-03-26 11:30:31 -0300

    Refactor MealPage.vue

--- src/pages/MealPage.vue
+++ src/pages/MealPage.vue
@@ -1,6 +1,5 @@
 <script setup>
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { onBeforeMount, onMounted, ref } from "vue";
+import { onBeforeMount, ref } from "vue";
 import getMealById from "../api/lookup";
 
 const props = defineProps({
@@ -12,8 +11,6 @@ const mealInstructions = ref([""]);
 const requirements = ref([]);
 const contentLoaded = ref(false);
 
-const onView = ref(false);
-
 onBeforeMount(async () => {
   meal.value = await getMealById(props.mealID);
 
@@ -22,6 +19,7 @@ onBeforeMount(async () => {
   //This loop aims to get all non-empty elements and sort the on a new
   //Array of Objects {ingredient : measurenent}, for better understanding.
 
+  let newReqs = [];
   for (let i = 1; i <= 20; i++) {
     //20 is the fixed value on the API
 
@@ -31,7 +29,7 @@ onBeforeMount(async () => {
     if (ingredient === "" && (measurement === "" || measurement === " "))
       continue;
     else {
-      requirements.value.push({
+      newReqs.push({
         ingredient: ingredient,
         measurement: measurement,
       });
@@ -43,105 +41,73 @@ onBeforeMount(async () => {
     mealInstructions.value = meal.value.strInstructions.split("\r\n");
   }
 
+  requirements.value = newReqs.filter((item) => item.ingredient !== null);
+
   //Sets the visibility of the wrapper element
   contentLoaded.value = true;
-});
-
-onMounted(() => {
-  const observer = new IntersectionObserver((e) => {
-    e.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("show");
-      }
-    });
-  });
-  const hidden = document.querySelectorAll(".hidden");
-  hidden.forEach((el) => observer.observe(el));
 });
 </script>
 
 <template>
   <div>
-    <div
-      class="wrapper"
-      :class="{ loading: !contentLoaded, loaded: contentLoaded }"
-    >
-      <header>
-        <h1 class="mealName">
-          <span class="icon">
-            <a @click="$router.back()" class="icon-link back">
-              <FontAwesomeIcon icon="fa-solid fa-arrow-left" />
-            </a>
-          </span>
-          {{ meal.strMeal }}
-          <span class="icon">
-            <a
-              :href="meal.strYoutube"
-              target="_blank"
-              class="icon-link youtube"
-            >
-              <FontAwesomeIcon icon="fa-brands fa-youtube" />
-            </a>
-          </span>
-        </h1>
-      </header>
-
-      <div class="container">
+    <Transition name="fade">
+      <div class="wrapper" v-show="contentLoaded">
         <div class="heading">
-          <div class="meal-image">
+          <h1>{{ meal.strMeal }}</h1>
+        </div>
+        <section class="content">
+          <div class="content__left">
             <figure class="image">
               <img
                 loading="lazy"
-                width="600"
-                height="600"
                 :src="meal.strMealThumb"
-                alt="meal.strMeal"
+                :alt="meal.strMeal"
+                width="500"
+                height="500"
               />
             </figure>
-          </div>
-          <div class="meal-info">
-            <div class="category">
+            <div class="content__left__details">
               <h2>Category</h2>
               <p>{{ meal.strCategory }}</p>
-            </div>
-            <div class="area">
+              <hr />
               <h2>Area</h2>
               <p>{{ meal.strArea }}</p>
             </div>
-            <!-- <div class="nav-buttons"> -->
-            <!--   <button> -->
-            <!--     <FontAwesomeIcon icon="fa-solid fa-arrow-left" />Previous -->
-            <!--   </button> -->
-            <!--   <button> -->
-            <!--     Next<FontAwesomeIcon icon="fa-solid fa-arrow-right" /> -->
-            <!--   </button> -->
-            <!-- </div> -->
+            <hr />
+            <div class="content__left__ingredients">
+              <h2>Ingredients</h2>
+              <ul>
+                <li v-for="item in requirements">
+                  <p>
+                    <span> {{ item.ingredient }} </span>
+                    <span v-if="item.measurement !== null">
+                      : {{ item.measurement }}
+                    </span>
+                  </p>
+                </li>
+              </ul>
+            </div>
           </div>
+          <div class="content__right">
+            <iframe
+              loading="lazy"
+              :src="meal.strYoutube"
+              frameborder="0"
+              width="900"
+              height="500"
+            ></iframe>
 
-          <!-- meal-info -->
-        </div>
-        <div class="content">
-          <div class="ingredients" :class="{ hidden: !onView }">
-            <h2>Ingredients</h2>
-            <ul>
-              <li v-for="item in requirements">
-                <p v-if="item.ingredient !== null">
-                  {{ item.ingredient }}: <span> {{ item.measurement }} </span>
-                </p>
-              </li>
-            </ul>
+            <div class="content__right__instructions">
+              <h2>Instructions</h2>
+              <p v-for="text in mealInstructions">
+                {{ text }}
+              </p>
+            </div>
           </div>
-          <!-- ingredients -->
-          <div class="instructions" :class="{ hidden: !onView }">
-            <h3>Instructions</h3>
-            <p v-for="text in mealInstructions">{{ text }} <br /></p>
-          </div>
-        </div>
-        <!-- content -->
+        </section>
+        <section class="recommendations"></section>
       </div>
-      <!-- container -->
-    </div>
-    <!-- wrapper -->
+    </Transition>
   </div>
 </template>
 
@@ -156,313 +122,160 @@ onMounted(() => {
   margin: 0;
 }
 
-h1,
-h2,
-h3,
-p,
-a,
-span,
-summary {
+h2 {
+  font-size: 2.5rem;
+}
+
+.wrapper {
+  display: grid;
+  grid-template-areas:
+    "heading"
+    "content"
+    "recommendations";
+  grid-template-rows: auto;
+  padding: 2em;
+  row-gap: 3em;
+}
+
+.heading {
   color: $black;
-}
-
-.icon-link {
-  color: $orange;
-  cursor: pointer;
-
-  &.back {
-    margin-right: 0.5em;
-  }
-
-  &.youtube {
-    margin-left: 0.5em;
-  }
-}
-
-li {
-  list-style: none;
-}
-
-.mealName {
+  grid-area: heading;
+  font-size: 3rem;
   text-align: center;
-  font-size: 4rem;
-  margin-top: 1em;
 
-  text-transform: uppercase;
+  display: flex;
+  flex-direction: column;
 }
 
-.loading {
-  opacity: 0;
-  transition: all 1s ease;
-}
-
-.loaded {
-  opacity: 1;
-  transition: all 1s ease;
-}
-
-.container {
-  .heading {
-    display: flex;
-    flex-direction: row;
-    align-content: center;
-    margin: 2em;
-    gap: 2em;
-
-    .meal-image {
-      box-shadow: -3px 8px 21px 0px rgba(25, 25, 25, 0.71);
-      -webkit-box-shadow: -3px 8px 21px 0px rgba(25, 25, 25, 0.71);
-      -moz-box-shadow: -3px 8px 21px 0px rgba(25, 25, 25, 0.71);
-
-      border-radius: 15px;
-
-      overflow: hidden;
-      .image {
-        border-radius: inherit;
-        img {
-          transform: scale(1.1);
-          width: 100%;
-          height: auto;
-
-          max-width: 100%;
-          max-height: 600px;
-        }
+.content {
+  display: grid;
+  grid-template-columns: auto 1fr;
+  column-gap: 3em;
+  justify-items: center;
+  &__left {
+    padding: 2em;
+    background-color: #002626;
+    color: $white;
+    figure {
+      img {
+        height: auto;
+        max-width: 100%;
       }
     }
+    hr {
+      margin: 1em 0;
+    }
 
-    .meal-info {
-      padding-left: 1em;
-
-      h2 {
-        text-transform: uppercase;
-        font-size: 3.5rem;
-      }
-
+    &__details {
+      margin-top: 1em;
       p {
-        text-transform: uppercase;
-        font-size: 2.5rem;
-        font-style: italic;
+        font-size: 1.5rem;
       }
     }
-
-    .nav-buttons {
-      display: flex;
-      flex-direction: row;
-      justify-content: space-evenly;
-
-      button {
-        border: none;
-        color: $orange;
-        background: none;
-        font-size: 2rem;
-      }
-    }
-
-    animation: pop-left 1s ease;
-
-    @keyframes pop-left {
-      from {
-        transform: translateX(-200px);
-        opacity: 0;
-      }
-      to {
-        transform: translateX(0);
-        opacity: 1;
-      }
-    }
-  }
-
-  .content {
-    display: flex;
-    flex-direction: column;
-    margin: 2em;
-
-    .ingredients {
-      background-color: $darkorange;
-      border-radius: 5px;
-      padding: 0 0.5em 1em 0.5em;
-
-      h2 {
-        color: $white2;
-        text-align: center;
-        text-transform: uppercase;
-        font-size: 4em;
-      }
+    &__ingredients {
       ul {
-        border-left: 4px solid $white;
-
-        margin-top: 0.75em;
-        padding-left: 1em;
-
-        -webkit-column-count: 2;
-        -moz-column-count: 2;
-        column-count: 2;
-
+        list-style: none;
         li {
           p {
-            color: $white;
-            font-size: 1.5em;
+            font-size: 1.5rem;
 
             span {
-              color: $white2;
-              font-style: italic;
-              opacity: 50%;
+              &:not(:first-child) {
+                font-style: italic;
+              }
             }
           }
         }
       }
     }
+  }
 
-    .instructions {
-      margin-top: 2em;
-      h3 {
-        margin: 1em;
-        font-size: 4em;
-        text-transform: uppercase;
-        text-align: center;
-      }
+  &__right {
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    width: 100%;
 
+    row-gap: 2em;
+
+    iframe {
+      width: 100%;
+      max-width: 100%;
+    }
+
+    &__instructions {
+      color: $black;
       p {
-        font-size: $text + 0.5;
         text-align: justify;
-        padding-left: 1rem;
-        border-left: 4px solid $orange;
+        font-size: 1.5rem;
+        text-wrap: wrap;
 
         &::first-letter {
-          font-size: $text + 0.85;
+          font-size: 2rem;
+        }
+
+        &:not(:first-child) {
+          margin-top: 1em;
         }
       }
     }
   }
 }
 
-.show {
-  animation: pop-right 1s ease;
-  @keyframes pop-right {
-    from {
-      transform: translateX(200px);
-      opacity: 0;
-    }
-    to {
-      transform: translateX(0);
-      opacity: 1;
-    }
-  }
-}
-
-@media screen and (min-width: $large) {
-  .youtube {
-    display: none;
+@media (max-width: 66em) {
+  .wrapper {
+    grid-template-columns: auto;
   }
 
   .content {
-    .ingredients {
-      ul {
-        li {
-          font-size: 1.7em;
-        }
-      }
-    }
+    row-gap: 3em;
+    grid-template-columns: 1fr;
   }
 }
 
-@media screen and (min-width: $medium) and (max-width: $big) {
-  .back,
-  .youtube {
-    display: none;
+@media (max-width: 35em) {
+  .wrapper {
+    justify-items: center;
+    justify-content: center;
   }
-
-  .mealName {
-    font-size: 4rem;
+  .heading {
+    font-size: 1.5rem;
   }
-}
+  .content {
+    justify-items: center;
+    justify-content: center;
+    grid-template-columns: 350px;
 
-@media screen and (min-width: $xsmall) and (max-width: $small) {
-  .back,
-  .youtube {
-    display: none;
-  }
-
-  .mealName {
-    font-size: 4rem;
-  }
-
-  .container {
-    .heading {
-      flex-direction: column;
-      text-align: center;
-    }
-  }
-}
-
-@media screen and (max-width: $xsmall) {
-  .back,
-  .youtube {
-    display: none;
-  }
-
-  .mealName {
-    font-size: 2.5rem;
-  }
-
-  .container {
-    .heading {
-      flex-direction: column;
-      text-align: center;
-
-      .meal-info {
-        padding: 0;
-        h2 {
-          font-size: 3em;
-        }
-        p {
-          font-size: 2em;
-        }
-      }
-    }
-
-    .content {
-      align-items: center;
-      .ingredients {
+    &__right {
+      &__instructions {
         h2 {
           text-align: center;
-          font-size: 3em;
         }
-
-        ul {
-          -webkit-column-count: 1;
-          -moz-column-count: 1;
-          column-count: 1;
-
-          li {
-            p {
-              font-size: 1.5em;
-              margin: 0.5em 0;
-            }
-          }
-        }
-      }
-
-      .instructions {
-        h3 {
-          margin: 0 0 1em 0;
-          text-align: center;
-          font-size: 2.5rem;
-        }
-
         p {
-          padding: 0;
-          font-size: $text-mobile;
-          margin-bottom: 0.75em;
-
-          border: none;
-
-          &::first-letter {
-            font-size: $text + 0.25;
-          }
+          font-size: 1.3rem;
         }
       }
     }
   }
+}
+
+.fade-enter-from {
+  opacity: 0;
+  transform: translateY(-100px);
+}
+
+.fade-enter-to {
+  transition:
+    opacity 1s ease,
+    transform 0.6s ease;
+}
+
+.fade-leave-from,
+.fade-leave-to {
+  transform: translateY(50px);
+  opacity: 0;
+  transition:
+    opacity 1s ease,
+    transform 0.6s ease;
 }
 </style>
